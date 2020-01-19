abf2a4bd43fd8daa5b362b8e91b2f784a6ff8b6f
Sakib Alamin <55255186+sakiib@users.noreply.github.com>
2020-01-19 18:53:52 +0600

    Update views.py

--- products/views.py
+++ products/views.py
@@ -6,6 +6,3 @@ from . models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
-
-def new(request):
-    products = Product.objects.all()
