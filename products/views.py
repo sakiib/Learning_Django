75ee997a651e824f86338ab1a3f3089c2354cced
Sakib Alamin <55255186+sakiib@users.noreply.github.com>
2020-01-19 18:35:51 +0600

    first update

--- products/views.py
+++ products/views.py
@@ -6,3 +6,6 @@ from . models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
+
+def new(request):
+    products = Product.objects.all()
